bc0f6989a042dc2e86e443c8aa4dee7d06c7d77b
Michele Bianchi <jazzinghen@gmail.com>
2018-07-04 16:24:06 +0900

    Moved some keys around to have smarter layers.
    Also cross arrow cluster instead of VIM arrows. This feels a little
    better~

--- keyboards/levinson/keymaps/scarborough_fair/keymap.c
+++ keyboards/levinson/keymaps/scarborough_fair/keymap.c
@@ -30,16 +30,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   |------+------+------+------+------+------|
  * | Tab  |   A  |   S  |   D  |   F  |   G  |   |   H  |   J  |   K  |   L  |   ;  |Enter |
  * |------+------+------+------+------+------|   |------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   |   N  |   M  |   ,  |   .  |   /  |  "   |
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   |   N  |   M  |   ,  |   .  |  Up  |  "   |
  * |------+------+------+------+------+------|   |------+------+------+------+------+------|
- * | Ctrl | GUI  | Alt  | Kana |Lower |Space |   |Space |Raise | Left | Down |  Up  |Right |
+ * | Ctrl | GUI  | Alt  | Kana |Lower |Space |   |Space |Raise |   /  | Left | Down |Right |
  * `-----------------------------------------'   `-----------------------------------------'
  */
 [_QWERTY] = LAYOUT_ortho_4x12( \
   KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT, \
-  KC_LCTL, KC_LGUI, KC_LALT, KC_KANA, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_QUOT, \
+  KC_LCTL, KC_LGUI, KC_LALT, KC_KANA, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT \
 ),
 
 /* Lower
@@ -48,34 +48,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   |------+------+------+------+------+------|
  * | Del  | Prev |Pl/Pau| Next |      |      |   |      |   _  |   +  |   {  |   }  |Enter |
  * |------+------+------+------+------+------|   |------+------+------+------+------+------|
- * |      |      |      |      |      |      |   |      |ISO ~ |ISO | |      |      |  |   |
+ * |      |      | Vol- | Vol+ |      |      |   |      |      |      |      | PgUp |  |   |
  * |------+------+------+------+------+------|   |------+------+------+------+------+------|
- * |      |      |      |      |      |      |   |      |      | Home | PgUp |PgDown| End  |
+ * |      |      |MuHen | Hen  |      |      |   |      |      |      | Home |PgDown| End  |
  * `-----------------------------------------'   `-----------------------------------------'
  */
 [_LOWER] = LAYOUT_ortho_4x12( \
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
   KC_DEL,  KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______, \
-  _______, _______, _______, _______, _______, _______, _______,S(KC_NUHS),S(KC_NUBS),_______, _______, KC_PIPE, \
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END \
+  _______, _______, KC_VOLD, KC_VOLU, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_PIPE, \
+  _______, _______, KC_MHEN, KC_HENK, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_END   \
 ),
 
 /* Raise
  * ,-----------------------------------------.   ,-----------------------------------------.
  * |   `  |   1  |   2  |   3  |   4  |   5  |   |   6  |   7  |   8  |   9  |   0  | Bksp |
  * |------+------+------+------+------+------|   |------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |   |  F6  |   -  |   =  |   [  |   ]  |Enter |
+ * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |   |      |   -  |   =  |   [  |   ]  |Enter |
  * |------+------+------+------+------+------|   |------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |   |  F12 |ISO # |ISO / |      |      |  \   |
+ * |      |  F6  |  F7  |  F8  |  F9  |  F10 |   |      |      |      |      |      |  \   |
  * |------+------+------+------+------+------|   |------+------+------+------+------+------|
- * |      |      |MuHen |  Hen |      |      |   |      |      | Next | Vol- | Vol+ | Play |
+ * |      |  F11 |  F12 |      |      |      |   |      |      |      |      |      |      |
  * `-----------------------------------------'   `-----------------------------------------'
  */
 [_RAISE] = LAYOUT_ortho_4x12( \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______, \
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, _______, _______, KC_BSLS, \
-  _______, _______, KC_MHEN, KC_HENK, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
+  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______, \
+  _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, _______, _______, _______, _______, KC_BSLS, \
+  _______, KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______, _______, _______, _______  \
 ),
 
 /* Adjust (Lower + Raise)
@@ -90,10 +90,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------'   `-----------------------------------------'
  */
 [_ADJUST] =  LAYOUT_ortho_4x12( \
-  _______, RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL, \
+  _______, RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,  \
   _______, _______, _______, _______, _______, AG_NORM, AG_SWAP, QWERTY,  RGB_HUI, RGB_HUD, RGB_TOG, _______, \
   _______, BL_TOGG, BL_INC,  BL_DEC,  BL_BRTG, _______, _______, _______, RGB_SAI, RGB_SAD, RGB_M_P, RGB_M_B, \
-  _______, _______, _______, _______, _______, _______, _______, _______, RGB_VAI, RGB_VAD, RGB_M_R, RGB_M_G \
+  _______, _______, _______, _______, _______, _______, _______, _______, RGB_VAI, RGB_VAD, RGB_M_R, RGB_M_G  \
 )
 
 
