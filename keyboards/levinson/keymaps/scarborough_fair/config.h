13b1d29e206b20aaf6baa4c89d60a9b700ffd17f
Michele Bianchi <jazzinghen@gmail.com>
2018-07-17 17:31:30 +0900

    Enabled RGB animations

--- keyboards/levinson/keymaps/scarborough_fair/config.h
+++ keyboards/levinson/keymaps/scarborough_fair/config.h
@@ -37,4 +37,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
+/* Add RGB effects support */
+#define RGBLIGHT_ANIMATIONS
+#define RGBLIGHT_HUE_STEP 8
+#define RGBLIGHT_SAT_STEP 8
+#define RGBLIGHT_VAL_STEP 8
+
 #endif
