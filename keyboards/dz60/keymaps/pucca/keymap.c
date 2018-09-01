6f0e2d69161f1be6f376235d3b208301a0489c3b
Jazzinghen <jazzinghen@gmail.com>
2018-09-01 11:06:35 +0900

    First proper pucca version :

--- keyboards/dz60/keymaps/pucca/keymap.c
+++ keyboards/dz60/keymaps/pucca/keymap.c
@@ -1,4 +1,24 @@
+/*
+ * 2018 Michele Bianchi and Martina Gatti
+ */
+
 #include QMK_KEYBOARD_H
+
+#undef PRODUCT
+#define PRODUCT         Pucca
+#undef DESCRIPTION
+#define DESCRIPTION     Pucca Keyboard :D
+
+#define PU_VU KC_AUDIO_VOL_UP
+#define PC_VD
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+#define _QWERTY 0
+#define _FN 1
+#define _ADJUST 16
 
 #define MODS_CTRL_MASK  (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
 
@@ -8,29 +28,51 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Qwerty
    * ,-----------------------------------------------------------------------------------------.
-   * | ` ~ |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |   Bkspc   |
+   * | Esc |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |   Bkspc   |
    * |-----------------------------------------------------------------------------------------+
    * | Tab    |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  [  |  ]  |    \   |
    * |-----------------------------------------------------------------------------------------+
-   * | Caps    |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |    Enter    |
+   * | FN      |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |    Enter    |
    * |-----------------------------------------------------------------------------------------+
    * | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  | RSh |  U  | DEL |
    * |-----------------------------------------------------------------------------------------+
-   * | Ctrl |  Cmd  |  Alt  |              Space                | RAlt | FN  |  L  |  D  |  R  |
+   * | Ctrl |  GUI  |  Alt  |              Space                | RAlt | FN  |  L  |  D  |  R  |
    * `-----------------------------------------------------------------------------------------'
    */
 
-  LAYOUT_2_shifts(
+  [_QWERTY] = LAYOUT_2_shifts(
       F(0), KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, ______, KC_BSPC,
       KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
-      KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
+      MO(_FN), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
       KC_LSFT, ______, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_DEL,
-      KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_SPC, KC_SPC, KC_RALT, MO(1), KC_LEFT, KC_DOWN, KC_RIGHT
+      KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_SPC, KC_SPC, KC_RALT, MO(_ADJUST), KC_LEFT, KC_DOWN, KC_RIGHT
       ),
 
   /* FN Layer
    * ,-----------------------------------------------------------------------------------------.
-   * | Esc |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  F7 |  F8 |  F9 | F10 | F11 | F12 |   RESET   |
+   * | ` ~ |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  F7 |  F8 |  F9 | F10 | F11 | F12 |   RESET   |
+   * |-----------------------------------------------------------------------------------------+
+   * |        |Prev|Pl/Pau|Next |     |     |     |     |     |     |     |      |      |      |
+   * |-----------------------------------------------------------------------------------------+
+   * |         |Vol+ |Vol- |Mute |     |     |     |     |     |     |     |     |             |
+   * |-----------------------------------------------------------------------------------------+
+   * |           |     |     |     |     |     |     |     |     |     |     |     |PG_UP|     |
+   * |-----------------------------------------------------------------------------------------+
+   * |      |       |       |                                   |     |      |Home |PG_DN|End  |
+   * `-----------------------------------------------------------------------------------------'
+   */
+
+  [_FN] = LAYOUT_2_shifts(
+      KC_GRV, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, KC_F11, KC_F12, ______, ______,
+      ______, KC_MPRV, KC_MPLY, KC_MNXT, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,
+      ______, KC_VOLU, KC_VOLD, KC_MUTE, ______, ______, ______, ______, ______, ______, ______, ______, ______,
+      ______, ______,  ______,  ______,  ______, ______, ______, ______, ______, ______, ______, ______, ______, KC_PGUP, ______,
+      ______, ______,  ______,  ______,  ______, ______, ______, ______, KC_HOME, KC_PGDOWN, KC_END
+      ),
+
+  /* ADJUST Layer
+   * ,-----------------------------------------------------------------------------------------.
+   * |     |     |     |     |     |     |     |     |     |     |     |     |     |   RESET   |
    * |-----------------------------------------------------------------------------------------+
    * |        |RBB T|RGB M| Hue+| Hue-| Sat+| Sat-| Val+| Val-|     |     |      |      |      |
    * |-----------------------------------------------------------------------------------------+
@@ -41,11 +83,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |      |       |       |                                   |     |      |     |     |     |
    * `-----------------------------------------------------------------------------------------'
    */
-
-  LAYOUT_directional(
-      KC_GRV, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, KC_F11, KC_F12, KC_TRNS, RESET,
+  [_ADJUST] = LAYOUT_2_shifts(
+      ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, RESET,
       ______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, ______, ______, ______, ______, ______,
-      ______, BL_TOGG, BL_STEP, BL_INC, BL_DEC, ______, ______, ______, ______, ______, ______, ______,
+      ______, BL_TOGG, BL_STEP, BL_INC, BL_DEC, ______, ______, ______, ______, ______, ______, ______, ______,
       ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,
       ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______
       ),
