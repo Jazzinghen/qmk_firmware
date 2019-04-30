a40223054e5afd5763951b689256feb27fe37449
Jazzinghen <jazzinghen@gmail.com>
2019-04-30 14:07:24 +0900

    Moved some stuff around, maybe this will be better.

--- keyboards/ergodash/rev2/keymaps/kneesocks/keymap.c
+++ keyboards/ergodash/rev2/keymaps/kneesocks/keymap.c
@@ -25,23 +25,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Qwerty
    * ,----------------------------------------------------------------------------------------------------------------------.
-   * | ESC  |   1  |   2  |   3  |   4  |   5  |   -  |                    |   =  |   6  |   7  |   8  |   9  |   0  | Bksp |
+   * | ESC  |   1  |   2  |   3  |   4  |   5  |   -  |                    |   =  |   6  |   7  |   8  |   9  |   0  |  \   |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * | Tab  |   Q  |   W  |   E  |   R  |   T  |   [  |                    |   ]  |   Y  |   U  |   I  |   O  |   P  |  \   |
+   * | Tab  |   Q  |   W  |   E  |   R  |   T  |   [  |                    |   ]  |   Y  |   U  |   I  |   O  |   P  |  /   |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
    * | Ctrl |   A  |   S  |   D  |   F  |   G  |  Del |                    | Bksp |   H  |   J  |   K  |   L  |   ;  |  "   |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  | Enter|                    | Space|   N  |   M  |   ,  |   .  |  Up  | Shift|
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-   * | ADJ  |  GUI |  Alt | AltGr|||||||| FUNC | Enter|      ||||||||      | Space| Enter|||||||| /    |Left  | Down | Right|
+   * | ADJ  |  GUI |  Alt | AltGr|||||||| FUNC | Enter|      ||||||||      | Space| Enter|||||||| FUNC |Left  | Down | Right|
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_QWERTY] = LAYOUT( \
-    F(0),    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS,                        KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,                        KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
+    F(0),    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS,                        KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS, \
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,                        KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_SLSH, \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_DEL ,                        KC_BSPC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_ENT ,                        KC_SPC , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_RSFT, \
-    ADJUST,  KC_LGUI, KC_LALT, KC_ALGR,          FUNC,    KC_ENT ,KC_ENT,          KC_SPC,KC_SPC , KC_ENT,           KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT  \
+    ADJUST,  KC_LGUI, KC_LALT, KC_ALGR,          FUNC,    KC_ENT ,KC_ENT,          KC_SPC,KC_SPC , KC_ENT,           FUNC,    KC_LEFT, KC_DOWN, KC_RGHT  \
   ),
 
   /* Function
