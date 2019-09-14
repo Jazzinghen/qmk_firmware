7ab64d3db0f27c12d799d508d10328471659c3c0
Jazzinghen <jazzinghen@gmail.com>
2019-09-14 16:57:44 +0900

    Added Japanese switch and fixed naming issue

--- keyboards/dz60/keymaps/Vaporblade/keymap.c
+++ keyboards/dz60/keymaps/Vaporblade/keymap.c
@@ -1,13 +1,8 @@
 /*
- * 2018 Michele Bianchi
+ * 2019 Michele Bianchi
  */
 
 #include QMK_KEYBOARD_H
-
-#undef PRODUCT
-#define PRODUCT         Vaporblade
-#undef DESCRIPTION
-#define DESCRIPTION     Next generation Pok3r
 
 // Fillers to make layering more clear
 #define _______ KC_TRNS
@@ -38,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------------------------------------------------------------------------------------+
    * | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |      RShift     |
    * |-----------------------------------------------------------------------------------------+
-   * | Ctrl |  GUI  |  Alt  |              Space                | RAlt  |  FN  |  ADJ |  RCtrl |
+   * | Ctrl |  GUI  |  Alt  |              Space                | RAlt  | ZKHK |  ADJ |  RCtrl |
    * `-----------------------------------------------------------------------------------------'
    */
 
@@ -47,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,   KC_Q,     KC_W,     KC_E,   KC_R,   KC_T,   KC_Y,     KC_U,     KC_I,     KC_O,         KC_P,     KC_LBRC, KC_RBRC, KC_BSLS,
       MO(_FN),  KC_A,     KC_S,     KC_D,   KC_F,   KC_G,   KC_H,     KC_J,     KC_K,     KC_L,         KC_SCLN,  KC_QUOT, KC_ENT,
       KC_LSFT,  XXXXXXX,  KC_Z,     KC_X,   KC_C,   KC_V,   KC_B,     KC_N,     KC_M,     KC_COMM,      KC_DOT,   KC_SLSH, KC_RSFT, XXXXXXX,
-      KC_LCTL,  KC_LGUI,  KC_LALT,  KC_SPC, KC_SPC, KC_SPC, KC_RALT,  MO(_FN),  XXXXXXX,  MO(_ADJUST),  KC_RCTL
+      KC_LCTL,  KC_LGUI,  KC_LALT,  KC_SPC, KC_SPC, KC_SPC, KC_RALT,  LALT(KC_ZKHK),  XXXXXXX,  MO(_ADJUST),  KC_RCTL
       ),
 
   /* FN Layer
